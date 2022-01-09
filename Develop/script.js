27c76b6fbf93374d36d4f50e5b2bea36af0cc82b
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-09 11:51:41 +0000

    password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numeric = "0123456789";
+const special = "!@£$%^&*+=";
+
+const passwordLength = prompt("Please enter password length:");
+const length = parseInt (passwordLength);
+
+if (length < 8 || length > 128 || isNaN(passwordLength)) {
+  alert("choose a different length.");
+  return;
+}
+
+
 
 function generatePassword
 // Write password to the #password input
